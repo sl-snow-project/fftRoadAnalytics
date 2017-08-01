58933d3b05e148cb1a9308ade90d9d18e4c56d12
Tatsuya Hanyu <s1210099@gmail.com>
2017-08-01 16:10:30 +0900

    before presentaion TODO: I should rewrite real pulse output

--- src/fftRoadAnalytics.py
+++ src/fftRoadAnalytics.py
@@ -6,13 +6,15 @@ from scipy.fftpack import fft
 from sklearn.preprocessing import normalize
 import pandas as pd
 import numpy as np
+import sys
+import os
 
 class road_aizu_fft:
-    def __init__(self, blocksize=200, normalize=True):
+    def __init__(self, blocksize=40, normalize=True):
         self.blocksize = blocksize
         self.normalize = True
     
-    def parse_fft(self, pulse_data, latitude, longitude, time):
+    def parse_fft(self, carname, pulse_data, latitude, longitude, time):
         #if isinstance(parse_data, list) != True:
         #   print("Please input list object")
 
@@ -29,33 +31,45 @@ class road_aizu_fft:
         pulse_sum = [sum(abs(pulse)).astype(np.float32) for pulse in window_fft]
         sum_sqare = [i**2 for i in pulse_sum]
 
+        original_pulse_sumarr = [sum(abs(i)) for i in window_pulse]
+
         longitude_v1 = list(longitude)
         latitude_v1  = list(latitude)
         time_arr_v1  = list(time)
+        carname_v1   = list(carname)
 
         longtitude_arr = [longitude_v1[i] for i in position_arr]
         latitude_arr   = [latitude_v1[i] for i in position_arr]
         time_arr       = [time_arr_v1[i] for i in position_arr]
+        carname_arr    = [carname_v1[i] for i in position_arr]
 
-        return [list(i) for i in zip(sum_sqare, latitude_arr, longtitude_arr, time_arr)]
+        return [list(i) for i in zip(carname_arr, time_arr, sum_sqare, original_pulse_sumarr, latitude_arr, longtitude_arr, window_pulse)]
 
+
+dirname = ""
 tmp = road_aizu_fft(blocksize=40)
-df = pd.read_csv(".//20170220.csv")
-
-#datetime   = df.measurement_ms
-
-#df.index = pd.DatetimeIndex(df.measurement_ms, name='on_create').tz_localize('UTC').tz_convert('Asia/Tokyo')
-#print(df)
+# df = pd.read_csv(".//sample.csv")
+df = pd.read_csv(sys.argv[1])
+print(sys.argv[1])
+try:
+   os.mkdir(sys.argv[2])
+   dirname = sys.argv[2]
+except:
+   os.mkdir("datetime")
+   dirname = "datetime"
 
 for carname in df.car_name.unique():
     print(carname)
 
-    uniqcar_df = df[df.car_name.str.contains(str(carname))]
-    datetime   = uniqcar_df.measurement_ms
+    #uniqcar_df = df[df.car_name.str.contains(carname) and carname.contains(df.car_name)]
+    uniqcar_df = df.query('car_name=="'+carname+'"')
+    unix_date  = uniqcar_df.measurement_ms
     latitude   = uniqcar_df.latitude
     longitude  = uniqcar_df.longitude
     z_vertical = uniqcar_df.accel_z_vertical-9.8
+    carid      = uniqcar_df.car_name
+    datetime   = pd.to_datetime(unix_date, unit="ms")
 
-    fft_response = tmp.parse_fft(z_vertical, latitude=list(latitude), longitude=list(longitude), time=datetime)
+    fft_response = tmp.parse_fft(pulse_data=z_vertical, carname=list(carid), latitude=list(latitude), longitude=list(longitude), time=datetime)
     response_uniqcar_df = pd.DataFrame(fft_response)
-    response_uniqcar_df.to_csv(str(carname) + '.csv')
+    response_uniqcar_df.to_csv(str(dirname)+"/"+str(carname) + '.csv')
---
+++ src/tmp.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:set fileencoding=utf-8:
+
+import sys
+import os
+
+print(sys.argv[1])
